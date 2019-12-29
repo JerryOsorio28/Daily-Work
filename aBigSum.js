7899b3957be43efe11ea47338eac444872fef3ba
JerryOsorio28 <jerryOsorio28@users.noreply.github.com>
2019-12-28 19:45:37 -0500

    Diagonal difference completed

---
+++ aBigSum.js
@@ -0,0 +1,5 @@
+// Complete the aVeryBigSum function below.
+function aVeryBigSum(ar) {
+    //the reduce method sums the values inside of an array
+    return ar.reduce((acc, curr) => acc + curr)
+}
---
+++ diagonalDifference.js
@@ -0,0 +1,18 @@
+function diagonalDifference(arr) {
+    //keeps a track of arrays length
+    let arr_length = arr[0].length;
+    //holds values summed from top-left to bottom-right
+    let leftSum = 0;
+    //holds values summed from top-right to bottom-left
+    let rightSum = 0;
+    //for loop that iterates from both top-left and top-right , and wors its way down
+    for(let i = 0, j = arr_length - 1; i < arr_length; i++, j--){
+      leftSum += arr[i][i];
+      rightSum += arr[i][j];
+    }
+      //subtracts both values
+      let difference = leftSum - rightSum
+      //returns the absolute number(for negative values)
+      return Math.abs(difference);
+  }
+  
