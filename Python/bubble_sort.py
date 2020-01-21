cf3d97d48df2c77e45cb3d1bf1d7bede58208625
JerryOsorio28 <jerryOsorio28@users.noreply.github.com>
2020-01-21 17:07:56 -0500

    finished bubble sort challenge

---
+++ Python/bubble_sort.py
@@ -0,0 +1,18 @@
+import random
+# Ceates a list of 50 numbers, starting at 1
+arr = list(range(1, 51))
+# shuffles arr list
+random.shuffle(arr)
+
+def bubble_sort(arr):
+    # first iterator is to keep a track of the numbers than has been sorted (bubbled up)
+    for i in range(0, len(arr) - 1):
+        # second iterator is to make a comparison between the current index and the index after that..
+        for j in range(0, len(arr) - 1 - i):
+            # if it is greater..
+            if arr[j] > arr[j + 1]:
+                # we swap places...
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+    return arr
+
+bubble_sort(arr)
