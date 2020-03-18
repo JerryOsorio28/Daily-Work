ad35507ad48d15d8a082e28588363f3e1fdf41bc
JerryOsorio28 <jerryOsorio28@users.noreply.github.com>
2020-03-18 17:08:19 -0400

    slightly optimized contiguous sub array solution

--- Javascript/contiguousSubArr.js
+++ Javascript/contiguousSubArr.js
@@ -1,7 +1,7 @@
-// Given an integer array nums, find the contiguous subarray (containing at least one number) 
-// which has the largest sum and return its sum.
-// Example: Input: [-2,1,-3,4,-1,2,1,-5,4], Output: 6
-// Explanation: [4,-1,2,1] has the largest sum = 6.
+// // Given an integer array nums, find the contiguous subarray (containing at least one number) 
+// // which has the largest sum and return its sum.
+// // Example: Input: [-2,1,-3,4,-1,2,1,-5,4], Output: 6
+// // Explanation: [4,-1,2,1] has the largest sum = 6.
 
 const largestSum = arr => {
     // sub array variable
@@ -9,24 +9,20 @@ const largestSum = arr => {
     // an array that keeps a track of reduced arrays
     let reducedValues = []
     // for loop to iterate over the arr to grab sub arrays
-    for(let i = 0; i < arr.length; i++){
+    while(arr.length > 0){
       // for loop to increase size of sub array
-      for(let j = i + 1; j < arr.length; j++){
+      for(let i in arr){
         // we grab the sub array
-        subArr.push(arr[j])
+        subArr.push(arr[i])
         // we reduce the sub array and we push the reduced value to the reduced array
         reducedValues.push(subArr.reduce((a, b) => a + b))
       }
       // we reinitialize the subArr to be empty
       subArr = []
+      // and we remove the first element on the arr
+      arr.shift()
     }
-    // since the first value was not pushed to the sub arr, we compare if it is greater than the max value found in the reduced arr and return it if it is...
-    if(Math.max(...reducedValues) > arr[0]){
-      return Math.max(...reducedValues)
-    // otherwise we return the first element in the arr
-    } else {
-      return arr[0]
-    }
+    return Math.max(...reducedValues)
   }
   
   largestSum([-2,1,-3,4,-1,2,1,-5,4])
