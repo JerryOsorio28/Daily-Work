b64759a7df0c5d86b919f29cdb87b31dbf1caa8f
JerryOsorio28 <jerryOsorio28@users.noreply.github.com>
2020-04-27 18:52:53 -0400

    added better comments

--- Leet_Code/slidingWindowMax.js
+++ Leet_Code/slidingWindowMax.js
@@ -19,30 +19,42 @@
 var maxSlidingWindow = function(nums, k) {
   //arr that holds the output
   let output = []
-  //var that holds the sum of the window
+  //var that holds the current max
   let max = 0
+  // keeps a track of index
   let index = 0
-  //while loop to iterate over arr that checks while index 1 - k <= nums length
+  //while true
   while(true){
-    //condition that checks if index 2 <= k...
+    //condition that checks if index is less than k
     if(index < k){
-      //var that holds the sum of the window += what's in current index
+      //if it is, we check if the incoming num is > than the max
       if(max < nums[index]){
+        // if it is we update it
         max = nums[index]
+      // otherwise, we check if max equals 0, and if so, if the incoming num is less than 0 (handles negative values)
       } else if(max === 0 && nums[index] < max){
+        // if true, update the max value
         max = nums[index]
       }
-      //increase index 2
+      //increase index by 1
       index++
+    // when index is equal k..
     } else {
+      // we push current max value
       output.push(max)
+      // we check if the length of the original arr is k..
       if(nums.length === k){
+        // if it is, we can assume we reached the last value on the original arr, so we break
         break
       }
+      // otherwise, we remove the first element of the arr
       nums.splice(0, 1)
+      // reset index to 0
       index = 0
+      // reset max to 0
       max = 0
     }
   }
+  // return our output
   return output
 };
