f3975d260f62540416bd2cf27c7529b5c7c8bbbf
JerryOsorio28 <jerryOsorio28@users.noreply.github.com>
2020-10-04 19:48:38 -0400

    attempted build palindrome challenge 2nd time, 10/11 tests pass

--- Code_Signal/buildPalindrome.js
+++ Code_Signal/buildPalindrome.js
@@ -4,10 +4,10 @@
 // -----------
 // For st = "abcdc", the output should be "abcdcba".
 
+// --------FIRST ATTEMPT--------------- 7/11 tests passed
 function buildPalindrome(st) {
   // we need a var to keep a track of the letters being added to the end of the string
   st = st.split('')
-  let addedLetters = ''
   let middle = Math.floor(st.length / 2)
   //what we wanna do is iterate from both ends of the str at the same time, we can use a for loop with 2 variables to do so, one starting at the beginning, another one at the end.
   for(let i = 0, j = st.length - 1; i <= middle; i++, j--){
@@ -19,4 +19,31 @@ function buildPalindrome(st) {
   }
   st = st.join('')
   return st
+}
+// --------SECOND ATTEMPT--------------- 10/11 tests passed
+function buildPalindrome(st) {
+  let strCopy = st
+  let letters = ''
+  
+  for(let i = 0; i < strCopy.length; i++){
+    let middle = Math.floor(strCopy.length / 2)
+    
+    let firstHalf = strCopy.split('')
+    firstHalf.length = middle
+    
+    let secondHalf = strCopy.split('')
+    secondHalf = secondHalf.splice(strCopy.length % 2 === 0 ? middle : middle+1, strCopy.length - 1)
+    
+    firstHalf = firstHalf.sort()
+    secondHalf = secondHalf.sort()
+    
+
+    if(firstHalf.join('') == secondHalf.join('')){
+      letters = letters.split('').reverse().join('')
+      return st += letters
+    } else {
+      letters += strCopy[i]
+      strCopy += strCopy[i]
+    }
+  }
 }
