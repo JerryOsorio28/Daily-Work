bd6942648995fd2d22938b0a60919f4a503f2521
JerryOsorio28 <jerryOsorio28@users.noreply.github.com>
2020-10-22 15:32:00 -0400

    finished sudoku challenge, needs optimization

--- Code_Signal/sudoku.js
+++ Code_Signal/sudoku.js
@@ -18,44 +18,109 @@
 // the output should be true;
 
 function sudoku(grid) {
-  // variable that stores the seen values in the iteration
-  let seenValues = {}
-  // let smallMatrix = []
-
-  // double for loop to check the rows of the matrix
+  let sudoku = `${grid}`
+  // checks if the rows and columns have valid sudoku solutions, returns false if it does not.
+  if(checkMatrix(grid) === false){
+      return false
+  }
+  
+  let smallMatrix = []
+  
   for(let i = 0; i < grid.length; i++){
+      let subArr = []
       for(let j = 0; j < grid.length; j++){
-          // check if the current element in the iteration has been seen..
-          if(seenValues[grid[i][j]]){
-              // if it has, we return false
-              return false
-          // if not seen, we want to add the to the seen object
+          subArr.push(grid[i][j])
+          if(smallMatrix.length < 3 || i == 3 || i == 6 || i == 9){
+            if(j == 2){
+                smallMatrix.push(subArr)
+                subArr = []
+            } else if(j == 5){
+                smallMatrix.push(subArr)
+                subArr = []
+            } else if(j == 8){
+                smallMatrix.push(subArr)
+                subArr = []
+            }
           } else {
-              seenValues[grid[i][j]] = grid[i][j] 
+            if(j == 2){
+              if(i <= 3){
+                smallMatrix[0].push(...subArr)
+                subArr = [] 
+              } else if(i > 3 && i <= 6){
+                smallMatrix[3].push(...subArr)
+                subArr = []
+              } else {
+                smallMatrix[6].push(...subArr)
+                subArr = []
+              }
+            } else if(j == 5){
+              if(i <= 3){
+                smallMatrix[1].push(...subArr)
+                subArr = []
+              } else if(i > 3 && i <= 6){
+                smallMatrix[4].push(...subArr)
+                subArr = []
+              } else {
+                smallMatrix[7].push(...subArr)
+                subArr = []
+              }
+            } else if(j == 8){
+              if(i <= 3){
+                smallMatrix[2].push(...subArr)
+                subArr = []
+              } else if(i > 3 && i <= 6){
+                smallMatrix[5].push(...subArr)
+                subArr = []
+              } else {
+                smallMatrix[8].push(...subArr)
+                subArr = []
+              }
+            }  
           }
       }
-      seenValues = {}
   }
-  seenValues = {}
-  
-  let counter = 0
-  while(counter < grid.length){
-      // second double for loop to check columns of the matrix
+  // checks if it exists valid 3x3 small matrixes solutions
+  if(checkMatrix(smallMatrix) === false){
+      return false
+  }
+
+  function checkMatrix(grid){
+      // variable that stores the seen values in the iteration
+      let seenValues = {}
+      // double for loop to check the rows of the matrix
       for(let i = 0; i < grid.length; i++){
-          // check if the current element in the iteration has been seen..
-          if(seenValues[grid[i][counter]]){
-              // if it has, we return false
-              return false 
-          // if not seen, we want to add the to the seen object
-          } else {
-              seenValues[grid[i][counter]] = grid[i][counter]
+          for(let j = 0; j < grid.length; j++){
+              // check if the current element in the iteration has been seen..
+              if(seenValues[grid[i][j]]){
+                  // if it has, we return false
+                  return false
+              // if not seen, we want to add the to the seen object
+              } else {
+                  seenValues[grid[i][j]] = grid[i][j] 
+              }
           }
+          seenValues = {}
       }
       seenValues = {}
-      counter++
+      
+      if(`${grid}` == sudoku){
+          let counter = 0
+          while(counter < grid.length){
+              // second double for loop to check columns of the matrix
+              for(let i = 0; i < grid.length; i++){
+                  // check if the current element in the iteration has been seen..
+                  if(seenValues[grid[i][counter]]){
+                      // if it has, we return false
+                      return false 
+                  // if not seen, we want to add the to the seen object
+                  } else {
+                      seenValues[grid[i][counter]] = grid[i][counter]
+                  }
+              }
+              seenValues = {}
+              counter++
+          }
+      }
   }
-  
-  
-  // return true
   return true
 }
